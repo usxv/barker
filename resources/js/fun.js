45f483d6c360aa5cc4702dd44f9920924408f5e3
Sumit Verma <138086515+usxv@users.noreply.github.com>
2024-01-24 11:42:45 +0530

    Add files via upload

---
+++ resources/js/fun.js
@@ -0,0 +1,6 @@
+function inrtxt(){
+    let val= document.getElementById("mndt").innerText= "Redirecting to AppStore"
+        }
+        function inrtxt1(){
+    let val= document.getElementById("mndt").innerText= "Redirecting to PlayStore"
+        }
